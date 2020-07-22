414ca3de38269d62b77ed55b8dfbeb12c661754c
Rohan Chawla <rohanchawla@Rohans-MacBook-Air.local>
2020-07-22 23:01:39 +0530

    add edit,update,delete report routes and dont let them change the date

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/public`));
 app.use(methodOverride('_method'));
+app.locals.moment = require('moment');
 
 //?passport config
 app.use(
--- routes/report.js
+++ routes/report.js
@@ -31,13 +31,69 @@ router.post('/', isLoggedIn, (req, res) => {
 					res.redirect('back');
 				} else {
 					patient.reports.push(report._id);
-					console.log(patient.reports);
 					patient.save();
 					res.redirect(
 						`/patients/${patient._id}`
 					);
 				}
 			});
+		}
+	});
+});
+//edit
+router.get('/:report_id/edit', isLoggedIn, (req, res) => {
+	Patient.findById(req.params.id, (err, patient) => {
+		if (err || !patient) {
+			console.log(err);
+			res.redirect('back');
+		} else {
+			Report.findById(
+				req.params.report_id,
+				(err, report) => {
+					if (err || !report) {
+						console.log(err);
+						res.redirect('back');
+					} else {
+						res.render('report/edit', {
+							report,
+							patient
+						});
+					}
+				}
+			);
+		}
+	});
+});
+//update
+router.put('/:report_id', isLoggedIn, (req, res) => {
+	Report.findById(req.params.report_id, (err, report) => {
+		if (err) {
+			res.redirect('back');
+		} else {
+			report.summary = req.body.summary;
+			report.save();
+			res.redirect(`/patients/${req.params.id}`);
+		}
+	});
+	// Report.findByIdAndUpdate(
+	// 	req.params.report_id,
+	// 	req.body.report,
+	// 	(err, updatedComment) => {
+	// 		if (err) {
+	// 			res.redirect('back');
+	// 		} else {
+	// 			res.redirect(`/patients/${req.params.id}`);
+	// 		}
+	// 	}
+	// );
+});
+//destroy
+router.delete('/:report_id', isLoggedIn, (req, res) => {
+	Report.findByIdAndRemove(req.params.report_id, err => {
+		if (err) {
+			res.redirect('back');
+		} else {
+			res.redirect(`/patients/${req.params.id}`);
 		}
 	});
 });
